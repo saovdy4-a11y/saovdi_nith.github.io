2963cd9a5b1a5bf20dc95ca67114d871dce000df
Vibaksanna <vibak011@gmail.com>
2026-07-10 14:41:27 +0700

    ci: add GitHub Actions deploy workflow + set base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/saovdi_nith.github.io/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
